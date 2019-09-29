f36237d460e7b8fc49aa33b6b8fed819ae9e8a08
Hin <nujaijnah@live.com>
2019-09-30 00:12:50 +0800

    login.vue

---
+++ note/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+html {
+    background: #fbfcfe;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+}
+</style>
---
+++ note/src/components/login/Login.vue
@@ -0,0 +1,62 @@
+<template lang="html">
+    <div class="container">
+        <div class="head">
+            <span>用户登录</span>
+        </div>
+        <Input class='input' v-model="username" placeholder="账号" clearable />
+        <Input class='input' v-model="password" placeholder="密码" clearable />
+        <Button class="button" type="success" :disabled="submitDisable">提交</Button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+       return {
+           username: '',
+           password: '',
+           submitDisable: true,
+       }
+   },
+    watch: {
+        username: 'userInfoChange',
+        password: 'userInfoChange',
+    },
+
+    methods: {
+        userInfoChange: function() {
+            if(this.username && this.password) {
+                this.submitDisable = false
+            } else {
+                this.submitDisable = true
+            }
+        }
+    }
+
+}
+</script>
+
+<style lang="css" scoped>
+    .container {
+        width: 600px;
+        height: 400px;
+        margin: 0 auto;
+        padding: 10px 20px;
+    }
+
+    .head {
+        font-size:26px;
+        margin-bottom: 60px;
+    }
+
+    .input {
+        display: block;
+        width: 280px;
+        height: 60px;
+        margin: 0 auto;
+        margin-bottom: 20px;
+    }
+
+    .button {
+        width: 280px;
+    }
+</style>
